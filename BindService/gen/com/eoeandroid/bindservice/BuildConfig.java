1afcd6ca376edc51ed51133d473da041ef0c7ae5
fantasyxc <fantasyxc@gmail.com>
2015-03-06 10:04:42 +0800

    remove study project

--- BindService/gen/com/eoeandroid/bindservice/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.eoeandroid.bindservice;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- BroadcastReceiver/gen/com/eoeandroid/broadcastreceiver/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.eoeandroid.broadcastreceiver;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
